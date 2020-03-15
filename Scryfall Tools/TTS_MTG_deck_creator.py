5ad9677dbec2c51dc295e01c1a1fbf9404985a56
Matthijs Platenburg <m.j.platenburg@gmail.com>
2020-03-15 18:22:57 +0100

    Add functionality for card identifier overrides
    - Rework main.py
    - Rework random_commander_deck.py
    - Rework scryfall_tools.py
    - Rework TTS_MTG_deck_creator.py
    - Add *.idea/ to .gitignore, exclude config_default.ini
    - Add constants.py
    - Add overrides.py

--- Scryfall Tools/TTS_MTG_deck_creator.py
+++ Scryfall Tools/TTS_MTG_deck_creator.py
@@ -1,21 +1,22 @@
-'''Creates the files needed for a Magic: The Gathering deck
+"""Creates the files needed for a Magic: The Gathering deck
 used in TableTop Simulator
-'''
+ """
 
-import scryfall_tools
-from scryfall_tools import card_sizes
-import dropbox_uploader
 import json
 import time
 from datetime import datetime
-import sys
 from io import BytesIO
-from PIL import Image
 from pathlib import Path
 from random import randint
 
-# Cardback and ad cards taken from https://deckmaster.info,
-# backed up to dropbox for persistency and loading issues
+from PIL import Image
+
+import scryfall_tools
+from constants import CARD_SIZES
+from dropbox_uploader import upload_to_dropbox
+
+# Card-back and ad cards taken from https://deckmaster.info.
+# Backed up to Dropbox for persistence and to avoid loading issues
 back_1_url = "https://www.dropbox.com/s/9aecuelfwga8bv4/back_1.jpg?dl=1"
 back_2_url = "https://www.dropbox.com/s/ral1ldk9odsycqd/back_2.jpg?dl=1"
 
@@ -62,16 +63,20 @@ def collect_ids(deck):
     for card in deck:
         if card['layout'] in ('transform', 'double_faced_token'):
             if card['id'] not in df_card_ids:
-                card_id = (int(len(df_card_ids) / 24) * 100 +
-                           len(df_card_ids) % 24 + 100)
+                card_id = (
+                        int(len(df_card_ids) / 24) * 100
+                        + len(df_card_ids) % 24 + 100
+                )
                 df_card_ids[card['id']] = card_id
                 df_unique_cards[card_id] = card
             else:
                 card_id = df_card_ids[card['id']]
         else:
             if card['id'] not in sf_card_ids:
-                card_id = (int(len(sf_card_ids) / 24) * 100 +
-                           len(sf_card_ids) % 24 + 100)
+                card_id = (
+                        int(len(sf_card_ids) / 24) * 100
+                        + len(sf_card_ids) % 24 + 100
+                )
                 sf_card_ids[card['id']] = card_id
                 sf_unique_cards[card_id] = card
             else:
@@ -131,12 +136,12 @@ def create_deck_image_containers(card_size):
     image_pages = int((len(sf_card_ids)) / 24)
     image_rest = (len(sf_card_ids)) % 24
     for _ in range(image_pages):
-        sf_images.append((Image.new('RGB',
-                                    [i * 5 for i in card_size])))
+        sf_images.append((Image.new('RGB', [i * 5 for i in card_size])))
     if image_rest > 0:
-        rows = int((image_rest) / 5) + 1
-        sf_images.append(Image.new('RGB',
-                                   [card_size[0] * 5, card_size[1] * rows]))
+        rows = int(image_rest / 5) + 1
+        sf_images.append(
+            Image.new('RGB', [card_size[0] * 5, card_size[1] * rows])
+        )
 
     # Pages for DFCs
     dfc_images = []
@@ -157,18 +162,26 @@ def create_deck_image_containers(card_size):
 
 
 def create_deck_images(card_size_text):
-    print("Creating base images...")
-    card_size = card_sizes[card_size_text]
+    print('Creating deck images...', flush=True)
+    card_size = CARD_SIZES[card_size_text]
     sf_images, df_images = create_deck_image_containers(card_size)
+
+    print('Single-faced cards')
 
     start = time.time()
     for card_id, card in sf_unique_cards.items():
-        # Get the basic information for drawing the image
+        print(
+            f'Handling {card["name"] + " - " + str(card_id) + "...":<80}',
+            end='\r',
+            flush=True
+        )
+
         image_page = int((card_id - 100) / 100)
         image_on_page = (card_id - 100 * (image_page + 1)) % 24
-        image_box = (card_size[0] * (image_on_page % 5),
-                     card_size[1] * int(image_on_page / 5))
-        print(f'Handling {card["name"]} - {card_id}...\t\t\t\t\t', end='\r')
+        image_box = (
+            card_size[0] * (image_on_page % 5),
+            card_size[1] * int(image_on_page / 5)
+        )
 
         front_uri = card['image_uris'][card_size_text]
 
@@ -178,43 +191,56 @@ def create_deck_images(card_size_text):
             time.sleep(0.1 - between)
         start = time.time()
         card_image = scryfall_tools.get_card_image(uri=front_uri)
-        sf_images[image_page].paste(Image.open(BytesIO(card_image)),
-                                    box=image_box)
-    print()
-
-    for card_id, card in df_unique_cards.items():
-        print(f'Handling {card["name"]} - {card_id}...\t\t\t\t\t', end='\r')
-        image_page = int((card_id - 100) / 100)
-        image_on_page = (card_id - 100 * (image_page + 1)) % 24
-        image_box = (card_size[0] * (image_on_page % 5),
-                     card_size[1] * int(image_on_page / 5))
-
-        if card['layout'] == 'meld':
-            front_uri = card['image_uris'][card_size_text]
-            back_uri = card['image_uris'][card_size_text]
-        if card['layout'] == 'transform':
-            front_uri = card['card_faces'][0]['image_uris'][card_size_text]
-            back_uri = card['card_faces'][1]['image_uris'][card_size_text]
-
-        # Make sure to leave 100 ms between Scryfall calls
-        # TODO: query multiple cards together
-        between = time.time() - start
-        if between < (0.1 * 2):
-            time.sleep((0.1 * 2) - between)
-        start = time.time()
-        card_image_front = scryfall_tools.get_card_image(uri=front_uri)
-        card_image_back = scryfall_tools.get_card_image(uri=back_uri)
-
-        df_images[image_page * 2].paste(
-            Image.open(BytesIO(card_image_front)),
-            box=image_box
-        )
-        df_images[image_page * 2 + 1].paste(
-            Image.open(BytesIO(card_image_back)),
+        sf_images[image_page].paste(
+            Image.open(BytesIO(card_image)),
             box=image_box
         )
 
-    print("Saving images...")
+    if len(df_unique_cards) > 0:
+        print('\n\nDouble-faced cards')
+
+        for card_id, card in df_unique_cards.items():
+            print(
+                f'Handling {card["name"] + " - " + str(card_id) + "...":<80}',
+                end='\r',
+                flush=True
+            )
+
+            image_page = int((card_id - 100) / 100)
+            image_on_page = (card_id - 100 * (image_page + 1)) % 24
+            image_box = (
+                card_size[0] * (image_on_page % 5),
+                card_size[1] * int(image_on_page / 5)
+            )
+
+            front_uri = ''
+            back_uri = ''
+            if card['layout'] == 'meld':
+                front_uri = card['image_uris'][card_size_text]
+                back_uri = card['image_uris'][card_size_text]
+            if card['layout'] == 'transform':
+                front_uri = card['card_faces'][0]['image_uris'][card_size_text]
+                back_uri = card['card_faces'][1]['image_uris'][card_size_text]
+
+            # Make sure to leave 100 ms between Scryfall calls
+            # TODO: query multiple cards together
+            between = time.time() - start
+            if between < (0.1 * 2):
+                time.sleep((0.1 * 2) - between)
+            start = time.time()
+            card_image_front = scryfall_tools.get_card_image(uri=front_uri)
+            card_image_back = scryfall_tools.get_card_image(uri=back_uri)
+
+            df_images[image_page * 2].paste(
+                Image.open(BytesIO(card_image_front)),
+                box=image_box
+            )
+            df_images[image_page * 2 + 1].paste(
+                Image.open(BytesIO(card_image_back)),
+                box=image_box
+            )
+
+    print('\n\nSaving images...', flush=True)
     sf_urls = save_deck_images(sf_images)
     df_urls = save_deck_images(df_images)
 
@@ -226,7 +252,7 @@ def save_deck_images(images):
     for i, image in enumerate(images):
         image_bytes = BytesIO()
         image.save(image_bytes, format='png')
-        dropbox_url = dropbox_uploader.upload_to_dropbox(
+        dropbox_url = upload_to_dropbox(
             image_bytes.getvalue(),
             f'TTS_{datetime.now():%Y-%m-%d-%H_%M_%S}.png'
         )
@@ -237,14 +263,12 @@ def save_deck_images(images):
     return urls
 
 
-def create_deck_jsons(decks, sf_urls, df_urls, path, name):
+def create_deck_json_files(decks, sf_urls, df_urls, path, name):
     # The base JSON container
-    base = {}
-    base['ObjectStates'] = []
+    base = {'ObjectStates': []}
 
     sf_pages = int((len(sf_card_ids)) / 24)
     last_rows = int((len(sf_card_ids) % 24) / 5) + 1
-    last_columns = 5 if last_rows > 1 else (len(sf_card_ids) % 24) % 5
 
     back_url = back_1_url if randint(0, 1000) < 1000 else back_2_url
     main_customdeck = {
@@ -259,6 +283,7 @@ def create_deck_jsons(decks, sf_urls, df_urls, path, name):
         if not sf_unique_cards[(i + 1) * 100]['layout'] == 'token'
     }
 
+    # FIXME: Fix this mess
     token_customdeck = {
         i + 1: {
             "NumWidth": 5,
@@ -269,15 +294,16 @@ def create_deck_jsons(decks, sf_urls, df_urls, path, name):
         for i, url in enumerate(sf_urls)
         # Ignore the page if it doesn't contain tokens
         if sf_unique_cards[
-            (i + 1) * 100 +  # Base count (left-topmost card)
-            (23 if i < sf_pages else  # right-bottommost card for a full page
-             (last_rows - 1) * 5 + (len(sf_card_ids) % 24) % 5 - 1)
-        ]['layout'] in ['token', 'emblem', 'meld']
+               (i + 1) * 100  # Base count (left-topmost card)
+               + (
+                   23 if i < sf_pages  # Right-bottommost card for a full page
+                   else (last_rows - 1) * 5 + (len(sf_card_ids) % 24) % 5 - 1
+               )]['layout'] in ['token', 'emblem', 'meld']
     }
 
     df_pages = int((len(df_card_ids)) / 24) + 1
     last_rows = int((len(df_card_ids) % 24) / 5) + 1
-    last_columns = 5 if last_rows > 1 else (len(df_card_ids) % 24) % 5
+
     df_customdeck = {
         i + 1: {
             "NumWidth": 5,
@@ -303,61 +329,74 @@ def create_deck_jsons(decks, sf_urls, df_urls, path, name):
     for i, (deck_name, deck, tokens, dfcs) in enumerate(decks):
         # The main deck
         if deck:
-            deck_container = {}
-            deck_container['Transform'] = {
-                "posX": i * 3, "posY": 1.0, "posZ": -0.0,
-                "rotX": 0, "rotY": 180, "rotZ": 180,
-                "scaleX": 1, "scaleY": 1, "scaleZ": 1
+            deck_container = {
+                'Transform': {
+                    "posX": i * 3, "posY": 1.0, "posZ": -0.0,
+                    "rotX": 0, "rotY": 180, "rotZ": 180,
+                    "scaleX": 1, "scaleY": 1, "scaleZ": 1
+                },
+                'Name': 'DeckCustom',
+                'Nickname': f'{deck_name}',
+                'CustomDeck': main_customdeck,
+                'ContainedObjects': [
+                    get_contained_object(
+                        sf_unique_cards[card_id]['name'], card_id
+                    )
+                    for card_id, amount in deck.items()
+                    for i in range(amount)
+                ],
+                'DeckIDs': [
+                    card_id
+                    for card_id, amount in deck.items()
+                    for i in range(amount)
+                ]
             }
-            deck_container['Name'] = 'DeckCustom'
-            deck_container['Nickname'] = f'{deck_name}'
-            deck_container['CustomDeck'] = main_customdeck
-            deck_container['ContainedObjects'] = [
-                get_contained_object(sf_unique_cards[card_id]['name'], card_id)
-                for card_id, amount in deck.items() for i in range(amount)
-            ]
-            deck_container['DeckIDs'] = [
-                card_id for card_id, amount in deck.items() for i in range(amount)
-            ]
 
             df_offset = 100 * (int(len(deck) / 24) + 1)
             deck_container['ContainedObjects'] += [
-                get_contained_object(df_unique_cards[card_id]['name'], card_id + df_offset)
+                get_contained_object(
+                    df_unique_cards[card_id]['name'],
+                    card_id + df_offset
+                )
                 for card_id, amount in dfcs.items() for i in range(amount)
             ]
             deck_container['DeckIDs'] += [
-                card_id + df_offset for card_id, amount in dfcs.items() for i in range(amount)
+                card_id + df_offset for card_id, amount in dfcs.items()
+                for i in range(amount)
             ]
 
             base['ObjectStates'] += [deck_container]
 
         # The token container
         if tokens:
-            token_deck = {}
-            token_deck['Transform'] = {
-                "posX": i * 3, "posY": 1.0, "posZ": 4.0,
-                "rotX": 0, "rotY": 180, "rotZ": 0,
-                "scaleX": 1, "scaleY": 1, "scaleZ": 1
+            token_deck = {
+                'Transform': {
+                    "posX": i * 3, "posY": 1.0, "posZ": 4.0,
+                    "rotX": 0, "rotY": 180, "rotZ": 0,
+                    "scaleX": 1, "scaleY": 1, "scaleZ": 1
+                },
+                'Name': 'DeckCustom',
+                'Nickname': f'{deck_name} [tokens]',
+                'CustomDeck': token_customdeck,
+                'ContainedObjects': [
+                    get_contained_object(sf_unique_cards[card_id]['name'],
+                                         card_id)
+                    for card_id, amount in tokens.items() for i in
+                    range(amount)
+                ],
+                'DeckIDs': [card_id for card_id, amount in tokens.items() for i
+                            in range(amount)]
             }
-            token_deck['Name'] = 'DeckCustom'
-            token_deck['Nickname'] = f'{deck_name} [tokens]'
-            token_deck['CustomDeck'] = token_customdeck
-            token_deck['ContainedObjects'] = [
-                get_contained_object(sf_unique_cards[card_id]['name'], card_id)
-                for card_id, amount in tokens.items() for i in range(amount)
-            ]
-            token_deck['DeckIDs'] = [
-                card_id for card_id, amount in tokens.items() for i in range(amount)
-            ]
             base['ObjectStates'] += [token_deck]
 
         # The DFC container
         if dfcs:
-            dfc_deck = {}
-            dfc_deck['Transform'] = {
-                "posX": i * 3, "posY": 1.0, "posZ": 8.0,
-                "rotX": 0, "rotY": 180, "rotZ": 0,
-                "scaleX": 1, "scaleY": 1, "scaleZ": 1
+            dfc_deck = {
+                'Transform': {
+                    "posX": i * 3, "posY": 1.0, "posZ": 8.0,
+                    "rotX": 0, "rotY": 180, "rotZ": 0,
+                    "scaleX": 1, "scaleY": 1, "scaleZ": 1
+                }
             }
             if len(dfcs) == 1:
                 dfc = list(dfcs.keys())[0]
@@ -370,39 +409,46 @@ def create_deck_jsons(decks, sf_urls, df_urls, path, name):
                 dfc_deck['Nickname'] = f'{deck_name} [dfc]'
                 dfc_deck['CustomDeck'] = df_customdeck
                 dfc_deck['ContainedObjects'] = [
-                    get_contained_object(df_unique_cards[card_id]['name'], card_id)
-                    for card_id, amount in dfcs.items() for i in range(amount)
+                    get_contained_object(
+                        df_unique_cards[card_id]['name'],
+                        card_id
+                    )
+                    for card_id, amount in dfcs.items() for _ in range(amount)
                 ]
                 dfc_deck['DeckIDs'] = [
-                    card_id for card_id, amount in dfcs.items() for i in range(amount)
+                    card_id for card_id, amount in dfcs.items()
+                    for _ in range(amount)
                 ]
 
             base['ObjectStates'] += [dfc_deck]
 
-    json_path = Path(path or Path().absolute(),
-                     f'{name}.json')
+    json_path = Path(path or Path().absolute(), f'{name}.json')
     with open(json_path, 'w') as outfile:
         json.dump(base, outfile, indent=2, ensure_ascii=False)
 
 
-def create_TTS_MTG_decks(decks, path='',
-                         card_size_text="normal"):
-    print("Gathering unique cards...")
+def create_tts_mtg_decks(decks, path='', card_size_text="normal"):
+    print('Gathering unique cards...', flush=True)
     mains_as_ids, tokens_as_ids, dfcs_as_ids = transform_decks(decks.values())
 
-    print("Creating deck images...")
     sf_urls, df_urls = create_deck_images(card_size_text)
 
     if not sf_urls and not df_urls:
-        print("Something went wrong with uploading the deck images.")
+        print('Something went wrong with uploading the deck images.')
         return
 
-    create_deck_jsons(
-        zip([deck for deck in decks],
+    print('Creating deck JSON files', flush=True)
+    create_deck_json_files(
+        zip(
+            [deck for deck in decks],
             mains_as_ids,
             tokens_as_ids,
-            dfcs_as_ids),
-        sf_urls, df_urls, path, list(decks.keys())[0]
+            dfcs_as_ids
+        ),
+        sf_urls,
+        df_urls,
+        path,
+        list(decks.keys())[0]
     )
 
-    return
+    print('Done!', flush=True)
---
+++ Scryfall Tools/constants.py
@@ -0,0 +1,8 @@
+CARD_SIZES = {
+    'png': (745, 1040),
+    'border_crop': (480, 680),
+    'art_crop': (0, 0),
+    'large': (672, 936),
+    'normal': (488, 680),
+    'small': (146, 204)
+}
