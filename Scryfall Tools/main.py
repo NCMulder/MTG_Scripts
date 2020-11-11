77aba7b366dc673e34c35dbbb29014ba4f3490f2
Niels Mulder <nc.mulder@outlook.com>
2020-11-11 11:29:56 +0100

    Cleaned main.py Removed some old code

--- Scryfall Tools/main.py
+++ Scryfall Tools/main.py
@@ -9,49 +9,6 @@ from limited_pools import get_limited_pool
 from random_commander_deck import create_random_commander_deck
 from scryfall_tools import get_collection
 
-modes = [
-    'random_commander',
-    'decklist',
-    'sealed_pool',
-    'draft_packs'
-]
-
-
-def main(
-        mode, output_path, size='normal',
-        query='', set_code=None, decklist_path=None
-):
-    decks = {}
-
-    if mode == 'random_commander':
-        decklist, deckname = create_random_commander_deck(
-            verbose=True,
-            q=query
-        )
-        if not decklist:
-            return
-        decks[deckname] = decklist
-    elif mode == 'decklist':
-        if not decklist_path:
-            raise TypeError('Please provide a decklist path')
-        with open(decklist_path, mode='r') as decklist_file:
-            # TODO: Make this its own file
-            # Handle sideboards, sets
-            deckname = os.path.splitext(os.path.basename(decklist_path))[0]
-            decklist_array = decklist_file.readlines()
-            # Flip decklist_array ['[amount] [cardname]']
-            # to dict {[cardname]: [amount]}
-            deck_dict = {
-                ' '.join(entry.split(' ')[1:]).strip()
-                : int(entry.split(' ')[0])
-                for entry in decklist_array
-                if entry.strip() and not entry.startswith('//')
-            }
-            decklist = get_collection(deck_dict)
-            decks[deckname] = decklist
-
-    elif mode == 'sealed_pool':
-        decks = get_sealed_pool(set_code=set_code)
 
 def decklist(args):
     # Handle sideboards, sets
